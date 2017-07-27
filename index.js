2bee0fa52330fcbf5bb2db6e00502a551913f428
Samuel Marks <samuelmarks@gmail.com>
2017-07-28 00:45:28 +1000

    Updating to new `models_and_routes` format

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
 const restify = require("restify");
 const restify_plugins_1 = require("restify-plugins");
 const Waterline = require("waterline");
@@ -38,18 +39,21 @@ exports.strapFramework = (kwargs) => {
         return next();
     }));
     const waterline_obj = new Waterline();
-    const tryTblInit = entity => model => kwargs.models_and_routes[entity].models
-        && (kwargs.models_and_routes[entity].models[model].identity
-            || kwargs.models_and_routes[entity].models[model].tableName) ?
-        waterline_obj.loadCollection(waterline_1.Collection.extend(kwargs.models_and_routes[entity].models[model])) : kwargs.logger.warn(`Not initialising: ${entity}.${model}`);
-    Object.keys(kwargs.models_and_routes).map(entity => {
-        if (kwargs.models_and_routes[entity].routes)
-            Object.keys(kwargs.models_and_routes[entity].routes).map(route => kwargs.models_and_routes[entity].routes[route](app, `${kwargs.root}/${entity}`));
-        if (kwargs.models_and_routes[entity].route)
-            Object.keys(kwargs.models_and_routes[entity].route).map(route => kwargs.models_and_routes[entity].route[route](app, `${kwargs.root}/${entity}`));
-        if (!kwargs.skip_db && kwargs.models_and_routes[entity].models)
-            Object.keys(kwargs.models_and_routes[entity].models).map(tryTblInit(entity));
-    });
+    const tryTblInit = (program, models_set, norm_set) => Object.keys(program).forEach(entity => program[entity] != null && (program[entity].identity || program[entity].tableName) ?
+        models_set.add(entity) && waterline_obj.loadCollection(waterline_1.Collection.extend(program[entity]))
+        : norm_set.add(entity));
+    const routes = new Set();
+    const models = new Set();
+    const norm = new Set();
+    for (const [fname, program] of kwargs.models_and_routes)
+        if (program != null)
+            if (fname.indexOf('model') > -1 && !kwargs.skip_db)
+                tryTblInit(program, models, norm);
+            else
+                routes.add(Object.keys(program).map((route) => program[route](app, `${kwargs.root}/${path_1.dirname(fname)}`)) && path_1.dirname(fname));
+    kwargs.logger.info('Registered routes:', Array.from(routes.keys()).join('; '), ';');
+    kwargs.logger.warn('Failed registering models:', Array.from(norm.keys()).join('; '), ';');
+    kwargs.logger.info('Registered models:', Array.from(models.keys()).join('; '), ';');
     if (kwargs.use_redis) {
         kwargs.redis_cursors.redis = new Redis(kwargs.redis_config);
         kwargs.redis_cursors.redis.on('error', err => {
@@ -62,9 +66,8 @@ exports.strapFramework = (kwargs) => {
         if (kwargs.start_app)
             app.listen(kwargs.listen_port, () => {
                 kwargs.logger.info('%s listening at %s', app.name, app.url);
-                return kwargs.callback != null ?
-                    kwargs.callback(null, app, Object.freeze([]), Object.freeze([]))
-                    : null;
+                return kwargs.callback == null ? null
+                    : kwargs.callback(null, app, Object.freeze([]), Object.freeze([]));
             });
         else if (kwargs.callback != null)
             return kwargs.callback(null, app, Object.freeze([]), Object.freeze([]));
@@ -76,18 +79,18 @@ exports.strapFramework = (kwargs) => {
         }
         else if (ontology == null || ontology.connections == null || ontology.collections == null
             || ontology.connections.length === 0 || ontology.collections.length === 0) {
-            kwargs.logger.error('ontology =', ontology);
+            kwargs.logger.error('ontology =', ontology, ';');
             const error = new TypeError('Expected ontology with connections & collections');
             if (kwargs.callback != null)
                 return kwargs.callback(error);
             throw error;
         }
         kwargs.collections = ontology.collections;
-        kwargs.logger.info('ORM initialised with collections:', Object.keys(kwargs.collections));
+        kwargs.logger.info('ORM initialised with collections:', Object.keys(kwargs.collections), ';');
         kwargs._cache['collections'] = kwargs.collections;
         if (kwargs.start_app)
             app.listen(process.env['PORT'] || 3000, () => {
-                kwargs.logger.info('%s listening from %s', app.name, app.url);
+                kwargs.logger.info('%s listening from %s;', app.name, app.url);
                 if (kwargs.onServerStart != null)
                     kwargs.onServerStart(app.url, ontology.connections, kwargs.collections, app, kwargs.callback == null ? () => { } : kwargs.callback);
                 else if (kwargs.callback != null)
